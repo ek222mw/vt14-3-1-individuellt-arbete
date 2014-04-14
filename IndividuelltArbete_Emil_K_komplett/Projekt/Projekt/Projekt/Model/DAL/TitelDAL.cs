37bd3e39504acd7ab3ee1a2ced8315ca155e8395
Emil Karlsson <ek222mw@student.lnu.se>
2014-04-14 13:37:35 +0200

    Komplett del2

---
+++ IndividuelltArbete_Emil_K_komplett/Projekt/Projekt/Projekt/Model/DAL/TitelDAL.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+
+namespace Projekt.Model.DAL
+{
+    public class TitelDAL
+    {
+        #region Connection
+        private static string _connectionString;
+
+        static TitelDAL()
+        {
+            //Denna kod hämtar ut anslutningssträngen från web.config
+            _connectionString = WebConfigurationManager.ConnectionStrings["IndividuelltArbeteConnectionString"].ConnectionString;
+        }
+
+        private static SqlConnection CreateConnection()
+        {
+            return new SqlConnection(_connectionString);
+        }
+        #endregion
+
+
+        #region Get all titels
+        public static IEnumerable<Title> GetTitels()
+        {
+            // Skapar och initierar ett anslutningsobjekt.
+            using (var conn = CreateConnection())
+            {
+                try
+                {
+                    // Skapar det List-objekt som initialt har plats för 100 referenser till Titel-objekt.
+                    var titels = new List<Title>(100);
+
+                    // Skapar och initierar ett SqlCommand-objekt som används till att 
+                    // exekveras specifierad lagrad procedur.
+                    var cmd = new SqlCommand("app Schema.usp_GetAllTitels", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Öppnar anslutningen till databasen.
+                    conn.Open();
+
+                    // Den lagrade proceduren innehåller en SELECT-sats som kan returnera flera poster varför
+                    // ett SqlDataReader-objekt måste ta hand om alla poster. Metoden ExecuteReader skapar ett
+                    // SqlDataReader-objekt och returnerar en referens till objektet.
+                    using (var reader = cmd.ExecuteReader())
+                    {
+                        // Tar reda på vilket index de olika kolumnerna har. Det är mycket effektivare att göra detta
+                        // en gång för alla innan while-loopen. Genom att använda GetOrdinal behöver du inte känna till
+                        // i vilken ordning de olika kolumnerna kommer, bara vad de heter.
+                        var titelIDIndex = reader.GetOrdinal("TitelID");
+                        var tekniskInfoIDIndex = reader.GetOrdinal("TekniskInfo");
+                        var beskrivningIndex = reader.GetOrdinal("Beskrivning");
+                        var titelIndex = reader.GetOrdinal("Titel");
+                        var produktionsarIndex = reader.GetOrdinal("Produktionsår");
+                        var produktionsbolagIndex = reader.GetOrdinal("Produktionsbolag");
+                        var genreIndex = reader.GetOrdinal("Genre");
+
+                        // Så länge som det finns poster att läsa returnerar Read true. Finns det inte fler 
+                        // poster returnerar Read false.
+                        while (reader.Read())
+                        {
+                            // Hämtar ut datat för en post. Använder GetXxx-metoder - vilken beror av typen av data.
+                            // Du måste känna till SQL-satsen för att kunna välja rätt GetXxx-metod.
+                            titels.Add(new Title
+                            {
+                                TitelID = reader.GetInt32(titelIDIndex),
+                                TekniskInfo = reader.GetString(tekniskInfoIDIndex),
+                                Beskrivning = reader.GetString(beskrivningIndex),
+                                Titel = reader.GetString(titelIndex),
+                                Produktionsar = reader.GetInt16(produktionsarIndex),
+                                Produktionsbolag = reader.GetString(produktionsbolagIndex),
+                                Genre = reader.GetString(genreIndex)
+                            });
+                        }
+                    }
+
+                    // Sätter kapaciteten till antalet element i List-objektet, d.v.s. avallokerar minne
+                    // som inte används.
+                    titels.TrimExcess();
+
+                    // Returnerar referensen till List-objektet med referenser till Titel-objekt.
+                    return titels;
+                }
+                catch
+                {
+                    throw new ApplicationException("An error occured while getting titels from the database.");
+                }
+            }
+        }
+        #endregion
+
+
+        #region Get a titel by id
+        public Title GetTitelById(int titelID)
+        {
+            using (SqlConnection connection = CreateConnection())
+            {
+                try
+                {
+
+                    SqlCommand cmd = new SqlCommand("app Schema.usp_GetTitelByID", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.AddWithValue("@TitelID", titelID);
+
+                    connection.Open();
+
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            var titelIndex = reader.GetOrdinal("Titel");
+                            var TekniskInfoIndex = reader.GetOrdinal("TekniskInfo");
+                            var produktionsarIndex = reader.GetOrdinal("Produktionsår");
+                            var ProduktionsbolagIndex = reader.GetOrdinal("Produktionsbolag");
+                            var beskrivningIndex = reader.GetOrdinal("Beskrivning");
+                            var GenreIndex = reader.GetOrdinal("Genre");
+
+                            return new Title
+                            {
+                                Titel = reader.GetString(titelIndex),
+                                TekniskInfo = reader.GetString(TekniskInfoIndex),
+                                Produktionsar = reader.GetInt16(produktionsarIndex),
+                                Produktionsbolag = reader.GetString(ProduktionsbolagIndex),
+                                Beskrivning = reader.GetString(beskrivningIndex),
+                                Genre = reader.GetString(GenreIndex)
+
+
+                            };
+                        }
+                    }
+                    return null;
+                }
+                catch
+                {
+                    throw new ApplicationException("Ett fel inträffade i data access layer.");
+                }
+            }
+
+        }
+        #endregion
+
+        #region Insert a titel
+        public void InsertTitel(Title titel)
+        {
+
+            // Skapar och initierar ett anslutningsobjekt.
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    // Skapar och initierar ett SqlCommand-objekt som används till att 
+                    // exekveras specifierad lagrad procedur.
+                    SqlCommand cmd = new SqlCommand("app Schema.usp_AddTitel", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    // Lägger till de paramterar den lagrade proceduren kräver. Använder här det effektiva sätttet att
+                    // göra det på - något "svårare" men ASP.NET behöver inte "jobba" så mycket.
+                    //cmd.Parameters.Add("@TitelID", SqlDbType.Int).Value = movie.TitelID;
+                    cmd.Parameters.Add("@Titel", SqlDbType.VarChar, 50).Value = titel.Titel;
+                    cmd.Parameters.Add("@TekniskInfo", SqlDbType.VarChar, 1000).Value = titel.TekniskInfo;
+                    cmd.Parameters.Add("@Produktionsar", SqlDbType.Int).Value = titel.Produktionsar;
+                    cmd.Parameters.Add("@Produktionsbolag", SqlDbType.VarChar, 40).Value = titel.Produktionsbolag;
+                    cmd.Parameters.Add("@Beskrivning", SqlDbType.VarChar, 1000).Value = titel.Beskrivning;
+                    cmd.Parameters.Add("@Genre", SqlDbType.VarChar, 20).Value = titel.Genre;
+
+
+
+                    // Den här parametern är lite speciell. Den skickar inte något data till den lagrade proceduren,
+                    // utan hämtar data från den. (Fungerar ungerfär som ref- och out-prameterar i C#.) Värdet 
+                    // parametern kommer att ha EFTER att den lagrade proceduren exekverats är primärnycklens värde
+                    // den nya posten blivit tilldelad av databasen.
+                    cmd.Parameters.Add("@TitelID", SqlDbType.Int, 4).Direction = ParameterDirection.Output;
+
+                    // Öppnar anslutningen till databasen.
+                    conn.Open();
+
+                    // Den lagrade proceduren innehåller en INSERT-sats och returnerar inga poster varför metoden 
+                    // ExecuteNonQuery används för att exekvera den lagrade proceduren.
+                    cmd.ExecuteNonQuery();
+
+                    // Hämtar primärnyckelns värde för den nya posten och tilldelar Customer-objektet värdet.
+                    titel.TitelID = (int)cmd.Parameters["@TitelID"].Value;
+                }
+                catch
+                {
+                    // Kastar ett eget undantag om ett undantag kastas.
+                    throw new ApplicationException("An error occured in the data access layer.");
+                }
+            }
+        }
+        #endregion
+
+        #region Update a titel
+        public void UpdateTitel(Title titel)
+        {
+            // Skapar och initierar ett anslutningsobjekt.
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("app Schema.usp_upTitel", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    cmd.Parameters.Add("@Titel", SqlDbType.VarChar, 50).Value = titel.Titel;
+                    cmd.Parameters.Add("@TekniskInfo", SqlDbType.VarChar, 1000).Value = titel.TekniskInfo;
+                    cmd.Parameters.Add("@Produktionsar", SqlDbType.Int).Value = titel.Produktionsar;
+                    cmd.Parameters.Add("@Produktionsbolag", SqlDbType.VarChar, 40).Value = titel.Produktionsbolag;
+                    cmd.Parameters.Add("@Beskrivning", SqlDbType.VarChar, 1000).Value = titel.Beskrivning;
+                    cmd.Parameters.Add("@Genre", SqlDbType.VarChar, 20).Value = titel.Genre;
+
+                    cmd.Parameters.Add("@TitelID", SqlDbType.Int, 4).Value = titel.TitelID;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch
+                {
+                    // Kastar ett eget undantag om ett undantag kastas.
+                    throw new ApplicationException("An error occured in the data access layer.");
+                }
+            }
+        }
+        #endregion
+
+        #region Delete a titel
+        public void DeleteTitel(int titelID)
+        {
+            // Skapar och initierar ett anslutningsobjekt.
+            using (SqlConnection conn = CreateConnection())
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("app Schema.usp_delTitel", conn);
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+
+                    cmd.Parameters.Add("@TitelID", SqlDbType.Int, 4).Value = titelID;
+
+                    conn.Open();
+
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch
+                {
+                    // Kastar ett eget undantag om ett undantag kastas.
+                    throw new ApplicationException("An error occured in the data access layer.");
+                }
+            }
+        }
+        #endregion
+    }
+}
---
+++ IndividuelltArbete_Emil_K_komplett/Projekt/Projekt/Projekt/Pages/TitelPages/Edit.aspx.cs
@@ -0,0 +1,173 @@
+﻿using Projekt.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.ModelBinding;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projekt.Pages.TitelPages
+{
+    public partial class Edit : System.Web.UI.Page
+    {
+        #region Session
+        private bool HasMessage
+        {
+            get
+            {
+                return Session["message"] != null;
+            }
+        }
+
+        private string Message
+        {
+            get
+            {
+                var message = Session["message"] as string;
+                Session.Remove("message");
+                return message;
+            }
+
+            set
+            {
+                Session["message"] = value;
+            }
+        }
+
+        #endregion
+
+        #region Fields
+        private Service _service;
+        #endregion
+
+        #region Properties
+        private Service Service
+        {
+            get
+            {
+                return _service ?? (_service = new Service());
+            }
+        }
+
+        public CheckBoxList CheckBoxes { get; set; }
+
+        #endregion
+
+        #region Page load
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ShowMessage.Text = Message;
+            ShowMessage.Visible = true;
+        }
+        #endregion
+
+        #region Navigation
+        protected void AddRedirectButton_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Titel");
+            Context.ApplicationInstance.CompleteRequest();
+        }
+        #endregion
+
+        #region Update
+
+        public Title TitelFormView_GetData([QueryString]int TitelID)
+        {
+            //iD = titleID;
+            //titleID = Convert.ToInt32(Request.QueryString["id"]);
+            return Service.GetTitel(TitelID);
+        }
+
+        public void TitelFormView_UpdateItem([QueryString]int TitelID) // Parameterns namn är samma som värdet DataKeyNames har.
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var titel = Service.GetTitel(TitelID);
+
+                    if (titel == null)
+                    {
+                        ModelState.AddModelError(String.Empty,
+                            String.Format("Titeln med ID {0} hittades inte.", TitelID));
+                        return;
+                    }
+
+                    var checkBoxList = (CheckBoxList)TitelFormView.FindControl("FormatCheckBoxList");
+                    int[] format_ids = checkBoxList.Items.Cast<ListItem>().Where(li => li.Selected).Select(li => int.Parse(li.Value)).ToArray();
+                    if (TryUpdateModel(titel))
+                    {
+
+                        titel.TitelID = TitelID;
+                        Service.SaveTitel(titel, format_ids);
+
+                    }
+                    Message = "Uppdateringen lyckades";
+                    Response.Redirect("Titel", false);
+                    Page.SetTempData("SuccessMessage", "Titeln uppdaterades.");
+                    Response.RedirectToRoute("Titel", new { id = titel.TitelID });
+                    Context.ApplicationInstance.CompleteRequest();
+                }
+                catch (Exception)
+                {
+                    Message = "Uppdateringen misslyckades";
+                    ModelState.AddModelError(String.Empty, "Ett oväntat fel inträffade då titeluppgiften skulle uppdateras.");
+                }
+            }
+        }
+
+        #region Checkbox
+        public IEnumerable<Formats> FormatCheckboxList_GetData()
+        {
+            return Service.GetFormats();
+        }
+
+        protected void FormatCheckBoxList_DataBinding(object sender, EventArgs e)
+        {
+            var checkBoxes = sender as CheckBoxList;
+            if (checkBoxes != null)
+            {
+                CheckBoxes = checkBoxes;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        //Metod som kryssar i de format filmen har när man ska redigera
+        protected void FormatCheckBoxList_DataBound(object sender, EventArgs e)
+        {
+            int titelID = Convert.ToInt32(Request.QueryString["titelID"]);
+            var checkBoxes = sender as CheckBoxList;
+            var format = Service.GetTitelFormatByTitelID(titelID).ToList();
+
+            foreach (var checkBox in checkBoxes.Items.Cast<ListItem>())
+            {
+                for (int i = 0; i < format.Count; i++)
+                {
+                    if (format[i].FormatID.ToString() == checkBox.Value)
+                    {
+                        checkBox.Selected = true;
+                    }
+                }
+            }
+        }
+
+        protected void CheckCustomValidator_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+
+            var checkBoxList = (CheckBoxList)TitelFormView.FindControl("FormatCheckBoxList");
+            var checkBoxChecked = checkBoxList.SelectedItem;
+
+            if (checkBoxChecked != null)
+            {
+                args.IsValid = true;
+            }
+            else
+            {
+                args.IsValid = false;
+            }
+        }
+    }
+}
